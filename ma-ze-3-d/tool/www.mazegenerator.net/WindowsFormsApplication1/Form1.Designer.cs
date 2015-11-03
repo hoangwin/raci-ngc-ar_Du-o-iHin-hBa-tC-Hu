6b33ce2dab29bfe19ad40cefcb484b59c6572212
hoangwin <hoang.nguyenmau@hotmail.com>
2015-11-03 16:03:53 +0700

    ok

--- ma-ze-3-d/tool/www.mazegenerator.net/WindowsFormsApplication1/Form1.Designer.cs
+++ ma-ze-3-d/tool/www.mazegenerator.net/WindowsFormsApplication1/Form1.Designer.cs
@@ -37,11 +37,13 @@ namespace WindowsFormsApplication1
             this.textBoxHeight = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(206, 259);
+            this.button1.Location = new System.Drawing.Point(192, 379);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 41);
             this.button1.TabIndex = 0;
@@ -52,32 +54,33 @@ namespace WindowsFormsApplication1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 112);
+            this.label1.Location = new System.Drawing.Point(12, 112);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.Size = new System.Drawing.Size(193, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "File Name";
+            this.label1.Text = "File Name(must have number";
             // 
-            // textBox1
+            // textBoxImageName
             // 
-            this.textBoxImageName.Location = new System.Drawing.Point(192, 112);
-            this.textBoxImageName.Name = "textBox1";
+            this.textBoxImageName.Location = new System.Drawing.Point(226, 112);
+            this.textBoxImageName.Name = "textBoxImageName";
             this.textBoxImageName.Size = new System.Drawing.Size(100, 22);
             this.textBoxImageName.TabIndex = 2;
             this.textBoxImageName.Text = "1.png";
             // 
-            // textBox2
+            // textBoxWidth
             // 
             this.textBoxWidth.Location = new System.Drawing.Point(192, 160);
-            this.textBoxWidth.Name = "textBox2";
+            this.textBoxWidth.Name = "textBoxWidth";
             this.textBoxWidth.Size = new System.Drawing.Size(100, 22);
             this.textBoxWidth.TabIndex = 3;
             this.textBoxWidth.Text = "20";
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBoxWidth_TextChanged);
             // 
-            // textBox3
+            // textBoxHeight
             // 
             this.textBoxHeight.Location = new System.Drawing.Point(192, 208);
-            this.textBoxHeight.Name = "textBox3";
+            this.textBoxHeight.Name = "textBoxHeight";
             this.textBoxHeight.Size = new System.Drawing.Size(100, 22);
             this.textBoxHeight.TabIndex = 4;
             this.textBoxHeight.Text = "20";
@@ -100,11 +103,31 @@ namespace WindowsFormsApplication1
             this.label3.TabIndex = 6;
             this.label3.Text = "Height";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 314);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "LevelEasy";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "out put file nam prefix";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(698, 533);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBoxHeight);
@@ -130,6 +153,8 @@ namespace WindowsFormsApplication1
         private System.Windows.Forms.TextBox textBoxHeight;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- ma-ze-3-d/tool/www.mazegenerator.net/WindowsFormsApplication1/Form1.cs
+++ ma-ze-3-d/tool/www.mazegenerator.net/WindowsFormsApplication1/Form1.cs
@@ -17,20 +17,41 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
+        void writefile(int i)
+        {
+            string Name = textBox1.Text + i.ToString();
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(Name + ".cs", true))
+            {
+
+                file.WriteLine("using UnityEngine;");
+                file.WriteLine("using System.Collections;");
+
+                file.WriteLine("public class " + Name + " : Level");
+                file.WriteLine("{");
+                file.WriteLine("public override void init()");
+                file.WriteLine("{");
+                //file.WriteLine("Debug.Log("Init Level 2");
+                loadImage(textBoxImageName.Text,file);
+                file.WriteLine("}");
+                file.WriteLine("}");
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            loadImage(textBoxImageName.Text);
+            int index = int.Parse(textBoxImageName.Text.Substring(0, textBoxImageName.TextLength -4)); 
+            writefile(index);
         }
 
         int pixelPerCell = 0;
-        void loadImage(string name)
+        void loadImage(string name, System.IO.StreamWriter file)
         {
-            Bitmap image1 = (Bitmap)Image.FromFile(name, true);
+            Bitmap image1 = (Bitmap)Image.FromFile(name, true);          
             System.Console.WriteLine("Width " + image1.Width);
-            System.Console.WriteLine("Height "+image1.Height);
-            pixelPerCell = (image1.Width - 2) /Int32.Parse(textBoxWidth.Text);
+            System.Console.WriteLine("Height " + image1.Height);
+            pixelPerCell = (image1.Width - 2) / Int32.Parse(textBoxWidth.Text);
             System.Console.WriteLine("pixelPerCell " + pixelPerCell);
-            Color c,cNorth,cEast,cSouth,cWest;
+            Color c, cNorth, cEast, cSouth, cWest;
             //chu y luc nao cung phai +1 de loai pixel dau tien ra
 
             /*
@@ -39,19 +60,26 @@ namespace WindowsFormsApplication1
 		    new IntVector2(0, -1),//north
 		    new IntVector2(-1, 0)//east
             */
-            for (int i = 0;i< Int32.Parse(textBoxHeight.Text);i++)
+            List<Point> listHint = new List<Point>();
+            file.WriteLine("array2D = new int[,] {");
+            
+            System.Console.WriteLine("array2D = new int[,] {");
+
+            for (int i = 0; i < Int32.Parse(textBoxHeight.Text); i++)
             {
                 System.Console.Write("{");
+                file.Write("{");
                 for (int j = 0; j < Int32.Parse(textBoxWidth.Text); j++)
                 {
                     //pixel o giua
                     int y = 1 + i * pixelPerCell + pixelPerCell / 2;//dien giua cua cell
                     int x = pixelPerCell * j + 1 + pixelPerCell / 2;//diem giua cua cell
-                    c = image1.GetPixel(y, x);
+                    c = image1.GetPixel(x, y);
                     //neu c = mau do thi co nghia no chinh la duong di
                     if (c.R == Color.Red.R && c.G == Color.Red.G && c.B == Color.Red.B)
-                    {//dung udong di
-                        //System.Console.WriteLine(i + "," + j + "=" + c);
+                    {//dung duong di
+                     //  System.Console.WriteLine(i + "," + j + "=" + c);
+                        listHint.Add(new Point(i, j));
                     }
                     //          cNorth1
                     //   cWest8          cEast2
@@ -65,7 +93,7 @@ namespace WindowsFormsApplication1
                     //fff ->trang
                     if (cNorth.R == 0)//co mau den
                     {
-                        direction += 1<<0;
+                        direction += 1 << 0;
                     }
                     if (cSouth.R == 0)//co mau den
                     {
@@ -80,14 +108,67 @@ namespace WindowsFormsApplication1
                         direction += 1 << 3;
                     }
                     if (j < (Int32.Parse(textBoxWidth.Text) - 1))
+                    {
                         System.Console.Write(direction + ",");
+                        file.Write(direction + ",");
+                    }
                     else
+                    {
                         System.Console.Write(direction);
-
+                        file.Write(direction);
+                    }
                 }
-                System.Console.Write("},");
-                System.Console.WriteLine();
+                System.Console.WriteLine("},");
+                file.WriteLine("},");
             }
+            System.Console.WriteLine("};");
+            file.WriteLine("};");
+
+            //tim begin end
+
+
+            for (int j = 0; j < Int32.Parse(textBoxWidth.Text); j++)
+                {
+                    int y = image1.Height;
+                    int x = pixelPerCell * j + 1 + pixelPerCell / 2;
+                
+                Color c1 = image1.GetPixel(x, 0);
+                Color c2 = image1.GetPixel(x, y-1);
+                if (c1.R == Color.Red.R && c1.G == Color.Red.G && c1.B == Color.Red.B)
+                {//dung duong di End
+
+                    System.Console.WriteLine("POS_END = new Vector2(" + j+"," + 0 +");");
+                    file.WriteLine("POS_END = new Vector2(" + j + "," + 0 + ");");
+                }
+
+                if (c2.R == Color.Red.R && c2.G == Color.Red.G && c2.B == Color.Red.B)
+                {//dung duong di End
+
+                    System.Console.WriteLine("POS_BEGIN= new Vector2(" + j + "," + (Int32.Parse(textBoxWidth.Text) -1) + ");");
+                    file.WriteLine("POS_BEGIN= new Vector2(" + j + "," + (Int32.Parse(textBoxWidth.Text) - 1) + ");");
+                }
+            }
+
+            for (int i = 0; i < listHint.Count; i++)
+            {
+                System.Console.Write("," + listHint[i].X + "," + listHint[i].Y);
+                if (i == 0)
+                    file.Write("HintArray = new int[] {"+ listHint[i].X + "," + listHint[i].Y); 
+                else
+                    file.Write("," + listHint[i].X + "," + listHint[i].Y);
+            }
+            file.WriteLine("};");
+            file.WriteLine("WIDTH = array2D.GetLength(1);");
+            file.WriteLine("HEIGHT = array2D.GetLength(0);");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxWidth_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
