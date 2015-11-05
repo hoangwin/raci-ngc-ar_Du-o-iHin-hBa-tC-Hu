c6068e85c43be8920368111dfe01d217c121c06f
hoangwin <hoang.nguyenmau@hotmail.com>
2015-11-05 08:00:02 +0700

    ok

---
+++ ma-ze-3-d/Maze2/Project.WP8/Saiyan Maze/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using Saiyan_Maze.Resources;
+
+namespace Saiyan_Maze
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
