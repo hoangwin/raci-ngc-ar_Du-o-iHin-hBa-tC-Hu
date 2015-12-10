557c9a36da8a9a88c2caa94b1f391ddcb1657805
hoangwin <hoang.nguyenmau@hotmail.com>
2015-12-10 10:36:50 +0700

    ok

--- tan-k-ci-ty-3-d/Assets/Scripts/Tank/TankMovement.cs
+++ tan-k-ci-ty-3-d/Assets/Scripts/Tank/TankMovement.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+using System.Collections;
 public class TankMovement : MonoBehaviour
 {
     public int m_PlayerNumber = 1;
@@ -16,12 +16,17 @@ public class TankMovement : MonoBehaviour
     private Rigidbody m_Rigidbody;         
     private float m_MovementInputValueY;
     private float m_MovementInputValueX;        
-    private float m_OriginalPitch;         
+    private float m_OriginalPitch;
 
-
+    private float m_timeChangeDirection;
+    private Vector3 m_LastPostion;
+    
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_timeChangeDirection = 4;
+        if (m_PlayerNumber == 0)
+            StartCoroutine(ChangDirectionMove());
     }
 
 
@@ -54,23 +59,54 @@ public class TankMovement : MonoBehaviour
 
     private void Update()
     {
+        if (m_PlayerNumber == 1 || m_PlayerNumber ==2)
+        updatePlayerInput();
+      
+    }
+    void updatePlayerInput()
+    {
         m_MovementInputValueY = 0;
         m_MovementInputValueX = 0;
         // Store the player's input and make sure the audio for the engine is playing.
-        if (Input.GetButton("Up"))// Input.GetAxis(m_MovementAxisName);
-            m_MovementInputValueY = 1;
-        else if (Input.GetButton("Down"))// Input.GetAxis(m_MovementAxisName);
+
+        // Debug.Log(Input.GetAxis(m_TurnAxisName));
+        //dpad in gamepad
+
+        //
+		 if (Input.GetButton("Up"))
+                m_MovementInputValueY = 1;
+            else if (Input.GetButton("Down"))// Input.GetAxis(m_MovementAxisName);
                 m_MovementInputValueY = -1;
-        if (Input.GetButton("Left"))//m_MovementInputValueX = Input.GetAxis(m_TurnAxisName);
-            m_MovementInputValueX = -1;
-        else if (Input.GetButton("Right"))// Input.GetAxis(m_MovementAxisName);
-            m_MovementInputValueX = 1;
+            if (Input.GetButton("Left"))//m_MovementInputValueX = Input.GetAxis(m_TurnAxisName);
+                m_MovementInputValueX = -1;
+            else if (Input.GetButton("Right"))// Input.GetAxis(m_MovementAxisName);
+                m_MovementInputValueX = 1;
+        if (m_MovementInputValueY == 0 && m_MovementInputValueX == 0)
+        {
+                m_MovementInputValueY = Input.GetAxis(m_MovementAxisName);
+		        m_MovementInputValueX = Input.GetAxis(m_TurnAxisName);
+		        if (Mathf.Abs(m_MovementInputValueY) > Mathf.Abs(m_MovementInputValueX))
+		        {
+					if(Mathf.Abs(m_MovementInputValueX)>0.5f)
+					{
+			            m_MovementInputValueY = m_MovementInputValueY / Mathf.Abs(m_MovementInputValueY);
+		    	        m_MovementInputValueX = 0;
+					}
+		        }
+		        else if (Mathf.Abs(m_MovementInputValueY) < Mathf.Abs(m_MovementInputValueX))
+		        {
+					if(Mathf.Abs(m_MovementInputValueY)>0.5f)
+					{
+			            m_MovementInputValueY = 0;//
+			            m_MovementInputValueX = m_MovementInputValueX / Mathf.Abs(m_MovementInputValueX);
+					}
+		        }
+        }
         //Debug.Log("m_MovementInputValueY :" + m_MovementInputValueY);
-        
+        // Debug.Log(m_MovementInputValueY);
         EngineAudio();
     }
-
-
+   
     private void EngineAudio()
     {
         // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
@@ -102,13 +138,31 @@ public class TankMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // Move and turn the tank.
-        Move();
-      //  Turn();
+        if (m_PlayerNumber == 1 || m_PlayerNumber == 2)
+        {
+            Move();
+            //  Turn();
+        }
+        else
+            MoveRandom();
+       // if(m_PlayerNumber ==0)
+      //  Debug.Log(m_Rigidbody.velocity);
+
     }
 
 
     private void Move()
     {
+        if (m_PlayerNumber == 0)
+        {
+            if (!checkCanMove())//khi khong di chuyen dc nua thi chuyen huong
+            {
+                Debug.Log("Can Chuyen Huong");
+                StopAllCoroutines();
+                m_timeChangeDirection = 0f;
+                StartCoroutine(ChangDirectionMove());
+            }
+        }
         // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
         Vector3 movement = Vector3.zero;
         if (m_MovementInputValueY < 0)
@@ -145,9 +199,53 @@ public class TankMovement : MonoBehaviour
             //Vector3 movement = transform. * m_TurnInputValue * m_Speed * Time.deltaTime;
 
             // Apply this movement to the rigidbody's position.
+            m_LastPostion = m_Rigidbody.transform.position;
             m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+       
     }
+    void MoveRandom()
+    {
+        Move();
+       //if( m_MovementInputValueY == 1+
+       //{
 
+       //}
+       // else if(m_MovementInputValueX ==1)
+       //{
+
+       // }
+    }
+    private IEnumerator ChangDirectionMove()
+    {
+        yield return new WaitForSeconds(m_timeChangeDirection);
+        m_timeChangeDirection = Random.Range(2f, 5f);
+            // If there isn't a winner yet, restart this coroutine so the loop continues.
+            // Note that this coroutine doesn't yield.  This means that the current version of the GameLoop will end.
+        Debug.Log("Change Direction");
+        int i = Random.Range(0,4); 
+        if(i ==0)
+        {
+            m_MovementInputValueX = 1;
+            m_MovementInputValueY = 0;
+        }
+        else if (i == 1)
+        {
+            m_MovementInputValueX = 0;
+            m_MovementInputValueY = 1;
+        }
+        else if (i == 2)
+        {
+            m_MovementInputValueX = 0;
+            m_MovementInputValueY = -1;
+        }
+        else if (i == 3)
+        {
+            m_MovementInputValueX = -1;
+            m_MovementInputValueY = 0;
+        }
+        StartCoroutine(ChangDirectionMove());
+      
+    }
 
     private void Turn()
     {
@@ -160,4 +258,18 @@ public class TankMovement : MonoBehaviour
         // Apply this rotation to the rigidbody's rotation.
         m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
     }
+    bool checkCanMove()
+    {
+        if (transform.position.Equals(m_LastPostion))
+            return false;
+        return true;
+    }
+    //void OnCollisionEnter(Collision collision)
+    //{
+       // Debug.Log("Enter Collider");
+       // StopAllCoroutines();
+      //  m_timeChangeDirection = 03f;
+      //  StartCoroutine(ChangDirectionMove());
+    //}
+    
 }
